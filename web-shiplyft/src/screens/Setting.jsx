e952753a9cb20f3d4d89fc7527c7179dd78828f8
gursewaksingh <gursewak.singh@nestorbird.com>
2024-12-15 01:15:09 +0530

    Dashboard sidebar changes applied

---
+++ web-shiplyft/src/screens/Setting.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+export default function Setting(){
+    return (
+        <div>
+            <h2>Settings</h2>
+            <p>Change your account settings</p>
+        </div>
+    )
+}
