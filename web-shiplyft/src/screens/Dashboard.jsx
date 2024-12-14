ec5bf42eb368d1e6ab6b2169d122b6d62177e3e1
gursewaksingh <gursewak.singh@nestorbird.com>
2024-12-14 10:22:35 +0530

    Dashboard Added

---
+++ web-shiplyft/src/screens/Dashboard.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import DashboardOuter from '../features/DashboardOuter';
+export default function Dashboard (){
+    return (
+        <div>
+            <DashboardOuter/>
+        </div>
+    )
+}
